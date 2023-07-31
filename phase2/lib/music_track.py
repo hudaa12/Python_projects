57f7cd2b608f0117afb0d4db9e62a29347c335a0
OriginalBinaryHustler <mr.watkin@live.co.uk>
2023-07-31 16:52:52 +0100

    31/07 Paired Project

--- phase2/lib/music_track.py
+++ phase2/lib/music_track.py
@@ -1,7 +1,49 @@
-class MusicTrack:
+class MusicTracker:
+
     def __init__(self):
-        self.track = ["calm down", "cupid"]
+        self.music_dict = {}
 
-    def add(self, track):
-        self.track.append()
+    def add_track(self, artist, track):
+        self.music_dict.setdefault(artist, []).extend([track])
+        return f'Your track: {track} has been added.'
 
+    def view_artists(self):
+        artist = self.music_dict.keys()
+        return '\n'.join(artist)
+
+    def view_tracks_by_artist(self, artist):
+        for k,v in self.music_dict.items():
+            if k == artist:
+                tracks = v
+         
+        return'\n'.join(tracks)
+
+    def view_library(self):
+
+        print((list(self.music_dict.items())))
+        return((list(self.music_dict.items())))
+
+    def remove_track(self, artist, track):
+        self.music_dict[artist].remove(track)
+        #self.music_dict[artist] = [value for value in self.music_dict[artist] if value != track]]
+        return  f'Track {track} has been removed'
+
+    def remove_artist(self, artist):
+        del self.music_dict[artist]
+        return f'Artist {artist} has been removed'
+
+    #def favourite_artist(self):
+        # for k,v in self.music_dict.items():
+        #     loop Throughfind len of v 
+            
+        #return f'Your favourite artist is {Tucpa} with {3} songs.'
+
+        # two variables to keep track of result: one is counter, one is artist name
+        # counter is 0 and artist name is empty string 
+        # music_dict => {artist : [songs,  songs ]}
+        # goal of the function: return the artist with the most number of songs
+        # loop through list of items with key value
+        # check number of entries in value 
+        # if current_num_songs > counter
+        #       set the counter to current_num_songs
+        #       set artist name to current key 
--- phase2/tests/test_music_track.py
+++ phase2/tests/test_music_track.py
@@ -4,6 +4,68 @@ from lib.music_track import *
 """
 initially, there are two tracks
 """
-def test_init_music_track():
-    tracker = MusicTrack()
-    assert tracker.() == []
+
+def test_add_and_view_artists():
+    library = MusicTracker()
+    library.add_track('Tupac','Hail Mairy')
+    answer = library.view_artists()
+    assert answer == 'Tupac'
+    library.add_track('Tupac','Hail Mairy')
+    library.add_track('Nipsey Hussle','Ocean Blues')
+    answer1 = library.view_artists()
+    assert answer1 == 'Tupac\nNipsey Hussle'
+    library.add_track('Tupac','Hail Mairy')
+    library.add_track('Nipsey Hussle','Ocean Blues')
+    library.add_track('Young Dolph','Hold Up')
+    library.add_track('King Von','I am')
+    answer1 = library.view_artists()
+    assert answer1 == 'Tupac\nNipsey Hussle\nYoung Dolph\nKing Von'
+
+
+def test_view_tracks_by_artist():
+    library = MusicTracker()
+    library.add_track('Tupac','Hail Mairy')
+    library.add_track('Nipsey Hussle','Ocean Blues')
+    library.add_track('Young Dolph','Hold Up')
+    library.add_track('Young Dolph','Crashin Out')
+    library.add_track('King Von','I am')
+    answer = library.view_tracks_by_artist('Young Dolph')
+    assert answer == 'Hold Up\nCrashin Out'
+
+def test_view_library():
+    library = MusicTracker()
+    library.add_track('Tupac','Hail Mairy')
+    library.add_track('Nipsey Hussle','Ocean Blues')
+    library.add_track('Young Dolph','Hold Up')
+    library.add_track('Young Dolph','Crashin Out')
+    library.add_track('King Von','I am')
+    answer = library.view_library()
+    assert answer == [('Tupac', ['Hail Mairy']), ('Nipsey Hussle', ['Ocean Blues']), ('Young Dolph', ['Hold Up', 'Crashin Out']), ('King Von', ['I am'])]
+
+def test_remove_track():
+    library = MusicTracker()
+    library.add_track('Tupac', 'Uppercut')
+    library.add_track('Tupac', 'Starin Through My Rearview')
+    ans = library.remove_track('Tupac', 'Uppercut')
+    assert ans == f'Track Uppercut has been removed'
+    answer = library.view_tracks_by_artist('Tupac')
+    assert answer == 'Starin Through My Rearview'
+
+def test_remove_artist():
+    library = MusicTracker()
+    library.add_track('Tupac', 'Uppercut')
+    library.add_track('Tupac', 'Starin Through My Rearview')
+    ans = library.remove_artist('Tupac')
+    assert ans == f'Artist Tupac has been removed'
+
+#def test_favourite_artist():
+    # library = MusicTracker()
+    # library.add_track('Tupac', 'Uppercut')
+    # library.add_track('Tupac', 'Starin Through My Rearview')
+    # library.add_track('Tupac','Hail Mairy')
+    # library.add_track('Nipsey Hussle','Ocean Blues')
+    # library.add_track('Young Dolph','Hold Up')
+    # library.add_track('Young Dolph','Crashin Out')
+    # library.add_track('King Von','I am')
+    # ans = library.favourite_artist()
+    # assert ans == f"Your favourite artist is Tupac with 3 songs."
